9119538fd7f2d34a0f21d857e56b8bab57205a4e
Ruben <gitgpg@rubend.nl>
2022-02-10 19:35:04 +0100

    Initial commit.

---
+++ src/main/java/net/cubekrowd/playerlist/Playerlist.java
@@ -0,0 +1,16 @@
+package net.cubekrowd.playerlist;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Playerlist extends Plugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
