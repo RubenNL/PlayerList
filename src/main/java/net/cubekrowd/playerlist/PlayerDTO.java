aae7873d5dcdf9b30883495a532ac138f5bbff44
Ruben <gitgpg@rubend.nl>
2022-02-12 11:14:35 +0100

    show head.

--- src/main/java/net/cubekrowd/playerlist/PlayerDTO.java
+++ src/main/java/net/cubekrowd/playerlist/PlayerDTO.java
@@ -3,11 +3,14 @@ package net.cubekrowd.playerlist;
 import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
+import java.util.UUID;
+
 @Getter
 public class PlayerDTO {
-	public String name;
+	private String name;
+	private UUID uuid;
 	public PlayerDTO(ProxiedPlayer player) {
 		this.name=player.getDisplayName();
-		this.name=player.getName();
+		this.uuid=player.getUniqueId();
 	}
 }
