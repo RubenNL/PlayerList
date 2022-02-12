2721bf085623065a426af2a07079b31b2ba4e6cb
Ruben van Dijk <15885455+RubenNL@users.noreply.github.com>
2022-02-12 11:05:16 +0100

    refactor to also show empty servers. (#3)

--- src/main/java/net/cubekrowd/playerlist/ServerDTO.java
+++ src/main/java/net/cubekrowd/playerlist/ServerDTO.java
@@ -1,19 +1,19 @@
 package net.cubekrowd.playerlist;
 
+import lombok.Getter;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 public class ServerDTO {
-	public String name;
-	//private List<ProxiedPlayer> players = new ArrayList<>();
-	public List<String> playerNames = new ArrayList<>();
+	private String name;
+	private List<PlayerDTO> players = new ArrayList<>();
 	public ServerDTO(String name) {
 		this.name=name;
 	}
 	public void addPlayer(ProxiedPlayer player) {
-//		this.players.add(player);
-		this.playerNames.add(player.getDisplayName());
+		this.players.add(new PlayerDTO(player));
 	}
 }
