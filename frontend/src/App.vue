914c5bb502b133a4b655a135af42ea15885fad95
Ruben <gitgpg@rubend.nl>
2022-02-12 12:02:53 +0100

    work on all urls. The data-URI can be added to the search.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div v-if="servers == null">Loading...</div>
+  <v-container v-if="!this.hasUrl">
+    <v-text-field v-model="url" />
+    <v-btn @click="get">go!</v-btn>
+  </v-container>
+  <div v-else-if="servers == null">Loading...</div>
   <v-expansion-panels v-else>
     <v-expansion-panel v-for="server in servers" :key="server.name">
       <v-expansion-panel-header>
@@ -9,12 +13,16 @@
         <v-list>
           <v-list-item v-for="player in server.players" :key="player.name">
             <v-list-item-content>
-              <v-container>
-                <v-img
-                  :src="`https://crafatar.com/avatars/${player.uuid}`"
-                  height="160px"
-                  width="160px"
+              <v-container align="center" justify="center">
+                <img
+                  style="image-rendering: pixelated"
+                  :src="`https://crafatar.com/avatars/${player.uuid}?size=8`"
+                  height="64px"
+                  width="64px"
+                  class="img"
+                  alt=""
                 />
+                <br />
                 {{ player.name }}
               </v-container>
             </v-list-item-content>
@@ -33,12 +41,22 @@ export default {
   data: () => {
     return {
       servers: null,
+      url: "",
+      hasUrl: false,
     };
   },
   mounted() {
-    fetch(settings.url + "/data.json")
-      .then((data) => data.json())
-      .then((data) => (this.servers = data));
+    this.url = settings.url;
+    if (window.location.search) this.url = window.location.search.substring(1);
+    if (this.url) this.get();
+  },
+  methods: {
+    get: function () {
+      this.hasUrl = true;
+      fetch(this.url + "/data.json")
+        .then((data) => data.json())
+        .then((data) => (this.servers = data));
+    },
   },
 };
 </script>
